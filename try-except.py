6630bca10543927cf757c6059dcd9dccc354c463
AnshuJoshi <geetanjalibca.Anshu@outlook.com>
2023-02-02 08:40:11 +0530

    first commit

---
+++ try-except.py
@@ -0,0 +1,10 @@
+#WAP to demonstrate try-except in python
+try:
+    numerator = 10
+    denominator = 0
+
+    result = numerator/denominator
+
+    print(result)
+except:
+    print("Error: Denominator cannot be 0.")
